43288aa08e7d72873b35ac56176fda196ef1478c
David Marino <davidjmarino8@gmail.com>
2023-01-15 14:22:03 -0500

    Iteration 2 complete

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,18 +8,31 @@ class Translator < English
   end
 
   def translate(input)
-    translation_text = input.each_char.inject([]) do |let1, let2|
-      let1 << @alphabet[let2]
+    array = []
+    input.chars.each do |char|
+      @alphabet.each do |eng_letter, braille_char|
+        if eng_letter == char
+          array << braille_char
+        end
+      end
+      @formatted = array.each_slice(40).map do |sliced|
+        sliced.transpose.map(&:join).join("\n")
+      end
     end
-    top = []
-    middle = []
-    bottom = []
-    translation_text.each do |letter|
-      top << letter[0]
-      middle << letter[1]
-      bottom << letter[2]
-    end
-    braille_arr = [top, middle, bottom]
-    braille_arr.map(&:join).join("\n")
+    @formatted.join("\n")
   end
 end
+
+
+# translation_text = input.each_char.inject([]) do |let1, let2|
+#   let1 << @alphabet[let2]
+# end
+# top = []
+# middle = []
+# bottom = []
+# translation_text.each do |letter|
+#   top << letter[0]
+#   middle << letter[1]
+#   bottom << letter[2]
+# end
+# braille_arr = "#{top.join}\n#{middle.join}\n#{bottom.join}\n"
